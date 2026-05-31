3a9d0cfb6f5ffa473665764989971b2a83d00757
Kemboi Elvis <kemboielvis22@gmail.com>
2026-05-31 23:42:28 +0300

    deps: update to v2025.12.6

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="goauthentik"
-AUTHENTIK_TAG="2026.2.2"
+AUTHENTIK_TAG="2025.12.6"
 
 # Create a new empty container image
 container=$(buildah from scratch)
